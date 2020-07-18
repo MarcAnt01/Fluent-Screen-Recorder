76fd4cddeddd267bb59a674ccb67905f98de807f
Filippo <fedeli.filippo@gmail.com>
2020-07-18 20:18:14 +0200

    Hide quality combobox with toggle on Thanks @LanceMcCarthy

---
+++ FluentScreenRecorder/InvertBoolConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+
+namespace FluentScreenRecorder
+{
+    public class InvertBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return Invert(value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return Invert(value);
+        }
+
+        private bool Invert(object value)
+        {
+            if (value is bool val)
+                return !val;
+
+            throw new ArgumentException("The incoming value must be a bool.");
+        }
+    }
+}
