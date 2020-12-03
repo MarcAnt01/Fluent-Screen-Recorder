4f763df00a866c0dd85178e59dad119c4eca8c6d
Filippo <fedeli.filippo@gmail.com>
2020-12-03 22:37:01 +0100

    1.4.5

---
+++ FluentScreenRecorder/AppCenterSecret.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace FluentScreenRecorder
+{
+    public partial class App
+    {
+        private const string APPCENTER_SECRET = "66766a12-7adb-4bdf-8321-00a81de1483e";
+    }
+}
+
