23a8f0912c413e788b587fcccd63d962812cb321
Filippo <fedeli.filippo@gmail.com>
2021-03-17 17:59:39 +0100

    Redone localization using ReswPlus

---
+++ FluentScreenRecorder/Strings/en-US/Resources.generated.cs
@@ -0,0 +1,403 @@
+// File generated automatically by ReswPlus. https://github.com/DotNetPlus/ReswPlus
+// The NuGet package ReswPlusLib is necessary to support Pluralization.
+using System;
+using Windows.ApplicationModel.Resources;
+using Windows.UI.Xaml.Markup;
+using Windows.UI.Xaml.Data;
+
+namespace FluentScreenRecorder.Strings{
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("DotNetPlus.ReswPlus", "2.1.3")]
+    [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+    [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
+    public static class Resources {
+        private static ResourceLoader _resourceLoader;
+        static Resources()
+        {
+            _resourceLoader = ResourceLoader.GetForViewIndependentUse("Resources");
+        }
+
+        #region About
+        /// <summary>
+        ///   Looks up a localized string similar to: About the app
+        /// </summary>
+        public static string About
+        {
+            get
+            {
+                return _resourceLoader.GetString("About");
+            }
+        }
+        #endregion
+
+        #region Bitrate
+        /// <summary>
+        ///   Looks up a localized string similar to: Bitrate
+        /// </summary>
+        public static string Bitrate
+        {
+            get
+            {
+                return _resourceLoader.GetString("Bitrate");
+            }
+        }
+        #endregion
+
+        #region BugReport
+        /// <summary>
+        ///   Looks up a localized string similar to: Report a bug
+        /// </summary>
+        public static string BugReport
+        {
+            get
+            {
+                return _resourceLoader.GetString("BugReport");
+            }
+        }
+        #endregion
+
+        #region Copyright
+        /// <summary>
+        ///   Looks up a localized string similar to: Copyright
+        /// </summary>
+        public static string Copyright
+        {
+            get
+            {
+                return _resourceLoader.GetString("Copyright");
+            }
+        }
+        #endregion
+
+        #region Delete
+        /// <summary>
+        ///   Looks up a localized string similar to: Delete
+        /// </summary>
+        public static string Delete
+        {
+            get
+            {
+                return _resourceLoader.GetString("Delete");
+            }
+        }
+        #endregion
+
+        #region Discord
+        /// <summary>
+        ///   Looks up a localized string similar to: Join our Discord server
+        /// </summary>
+        public static string Discord
+        {
+            get
+            {
+                return _resourceLoader.GetString("Discord");
+            }
+        }
+        #endregion
+
+        #region ExternalAudio
+        /// <summary>
+        ///   Looks up a localized string similar to: External Audio Recording
+        /// </summary>
+        public static string ExternalAudio
+        {
+            get
+            {
+                return _resourceLoader.GetString("ExternalAudio");
+            }
+        }
+        #endregion
+
+        #region Failure
+        /// <summary>
+        ///   Looks up a localized string similar to: Failure
+        /// </summary>
+        public static string Failure
+        {
+            get
+            {
+                return _resourceLoader.GetString("Failure");
+            }
+        }
+        #endregion
+
+        #region Framerate
+        /// <summary>
+        ///   Looks up a localized string similar to: Framerate
+        /// </summary>
+        public static string Framerate
+        {
+            get
+            {
+                return _resourceLoader.GetString("Framerate");
+            }
+        }
+        #endregion
+
+        #region GalleryView
+        /// <summary>
+        ///   Looks up a localized string similar to: Gallery View
+        /// </summary>
+        public static string GalleryView
+        {
+            get
+            {
+                return _resourceLoader.GetString("GalleryView");
+            }
+        }
+        #endregion
+
+        #region GitHub
+        /// <summary>
+        ///   Looks up a localized string similar to: View GitHub repository
+        /// </summary>
+        public static string GitHub
+        {
+            get
+            {
+                return _resourceLoader.GetString("GitHub");
+            }
+        }
+        #endregion
+
+        #region InternalAudio
+        /// <summary>
+        ///   Looks up a localized string similar to: Internal Audio Recording
+        /// </summary>
+        public static string InternalAudio
+        {
+            get
+            {
+                return _resourceLoader.GetString("InternalAudio");
+            }
+        }
+        #endregion
+
+        #region Ok
+        /// <summary>
+        ///   Looks up a localized string similar to: Ok
+        /// </summary>
+        public static string Ok
+        {
+            get
+            {
+                return _resourceLoader.GetString("Ok");
+            }
+        }
+        #endregion
+
+        #region Options
+        /// <summary>
+        ///   Looks up a localized string similar to: You can choose among the following options
+        /// </summary>
+        public static string Options
+        {
+            get
+            {
+                return _resourceLoader.GetString("Options");
+            }
+        }
+        #endregion
+
+        #region Privacy
+        /// <summary>
+        ///   Looks up a localized string similar to: Privacy policy
+        /// </summary>
+        public static string Privacy
+        {
+            get
+            {
+                return _resourceLoader.GetString("Privacy");
+            }
+        }
+        #endregion
+
+        #region Recording
+        /// <summary>
+        ///   Looks up a localized string similar to: Recording
+        /// </summary>
+        public static string Recording
+        {
+            get
+            {
+                return _resourceLoader.GetString("Recording");
+            }
+        }
+        #endregion
+
+        #region Resolution
+        /// <summary>
+        ///   Looks up a localized string similar to: Resolution
+        /// </summary>
+        public static string Resolution
+        {
+            get
+            {
+                return _resourceLoader.GetString("Resolution");
+            }
+        }
+        #endregion
+
+        #region Save
+        /// <summary>
+        ///   Looks up a localized string similar to: Save
+        /// </summary>
+        public static string Save
+        {
+            get
+            {
+                return _resourceLoader.GetString("Save");
+            }
+        }
+        #endregion
+
+        #region SaveAs
+        /// <summary>
+        ///   Looks up a localized string similar to: Save as
+        /// </summary>
+        public static string SaveAs
+        {
+            get
+            {
+                return _resourceLoader.GetString("SaveAs");
+            }
+        }
+        #endregion
+
+        #region SaveConfirm
+        /// <summary>
+        ///   Looks up a localized string similar to: Do you want to save your file?
+        /// </summary>
+        public static string SaveConfirm
+        {
+            get
+            {
+                return _resourceLoader.GetString("SaveConfirm");
+            }
+        }
+        #endregion
+
+        #region Share
+        /// <summary>
+        ///   Looks up a localized string similar to: Share
+        /// </summary>
+        public static string Share
+        {
+            get
+            {
+                return _resourceLoader.GetString("Share");
+            }
+        }
+        #endregion
+
+        #region SourceSizeToggle
+        /// <summary>
+        ///   Looks up a localized string similar to: Use source size as video resolution
+        /// </summary>
+        public static string SourceSizeToggle
+        {
+            get
+            {
+                return _resourceLoader.GetString("SourceSizeToggle");
+            }
+        }
+        #endregion
+
+        #region Version
+        /// <summary>
+        ///   Looks up a localized string similar to: Version
+        /// </summary>
+        public static string Version
+        {
+            get
+            {
+                return _resourceLoader.GetString("Version");
+            }
+        }
+        #endregion
+
+        #region VideoPreview
+        /// <summary>
+        ///   Looks up a localized string similar to: Video Preview
+        /// </summary>
+        public static string VideoPreview
+        {
+            get
+            {
+                return _resourceLoader.GetString("VideoPreview");
+            }
+        }
+        #endregion
+
+        #region Whatsnew
+        /// <summary>
+        ///   Looks up a localized string similar to: What's new?
+        /// </summary>
+        public static string Whatsnew
+        {
+            get
+            {
+                return _resourceLoader.GetString("Whatsnew");
+            }
+        }
+        #endregion
+    }
+
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("DotNetPlus.ReswPlus", "2.1.3")]
+    [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+    [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
+    [MarkupExtensionReturnType(ReturnType = typeof(string))]
+    public class ResourcesExtension: MarkupExtension
+    {
+        public enum KeyEnum
+        {
+            __Undefined = 0,
+            About,
+            Bitrate,
+            BugReport,
+            Copyright,
+            Delete,
+            Discord,
+            ExternalAudio,
+            Failure,
+            Framerate,
+            GalleryView,
+            GitHub,
+            InternalAudio,
+            Ok,
+            Options,
+            Privacy,
+            Recording,
+            Resolution,
+            Save,
+            SaveAs,
+            SaveConfirm,
+            Share,
+            SourceSizeToggle,
+            Version,
+            VideoPreview,
+            Whatsnew,
+        }
+
+        private static ResourceLoader _resourceLoader;
+        static ResourcesExtension()
+        {
+            _resourceLoader = ResourceLoader.GetForViewIndependentUse("Resources");
+        }
+        public KeyEnum Key { get; set;}
+        public IValueConverter Converter { get; set;}
+        public object ConverterParameter { get; set;}
+        protected override object ProvideValue()
+        {
+            string res;
+            if(Key == KeyEnum.__Undefined)
+            {
+                res = "";
+            }
+            else
+            {
+                res = _resourceLoader.GetString(Key.ToString());
+            }
+            return Converter == null ? res : Converter.Convert(res, typeof(String), ConverterParameter, null);
+        }
+    }
+} //FluentScreenRecorder.Strings
