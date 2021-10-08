7f75c9245ac3cf3224d53c0ed9671ae586c3a956
Filippo <fedeli.filippo@gmail.com>
2021-10-08 19:23:37 +0200

    Improvements/fixes for the media player Added tooltips, close #80

--- FluentScreenRecorder/CustomMediaTransportControls.cs
+++ FluentScreenRecorder/CustomMediaTransportControls.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Windows.UI.Xaml;
+using Windows.UI.Xaml.Automation;
 using Windows.UI.Xaml.Controls;
 
 namespace FluentScreenRecorder
@@ -8,7 +9,7 @@ namespace FluentScreenRecorder
     {
         public CustomMediaTransportControls()
         {
-            this.DefaultStyleKey = typeof(CustomMediaTransportControls);
+            DefaultStyleKey = typeof(CustomMediaTransportControls);
         }
 
         public event EventHandler Deleted;
@@ -20,10 +21,22 @@ namespace FluentScreenRecorder
             // This is where you would get your custom button and create an event handler for its click method.
             Button DeleteButton = GetTemplateChild("DeleteButton") as Button;            
             DeleteButton.Click += DeleteButton_Click;
+            ToolTip toolTip1 = new ToolTip();
+            toolTip1.Content = Strings.Resources.Delete;
+            ToolTipService.SetToolTip(DeleteButton, toolTip1);
+            AutomationProperties.SetName(DeleteButton, Strings.Resources.Delete);
             Button InfoButton = GetTemplateChild("InfoButton") as Button;
             InfoButton.Click += InfoButton_Click;
+            ToolTip toolTip2 = new ToolTip();
+            toolTip2.Content = Strings.Resources.Info;
+            ToolTipService.SetToolTip(InfoButton, toolTip2);
+            AutomationProperties.SetName(InfoButton, Strings.Resources.Info);
             Button ShareButton = GetTemplateChild("ShareButton") as Button;
             ShareButton.Click += Share_Click;
+            ToolTip toolTip3 = new ToolTip();
+            toolTip3.Content = Strings.Resources.Share;
+            ToolTipService.SetToolTip(ShareButton, toolTip3);
+            AutomationProperties.SetName(ShareButton, Strings.Resources.Share);
 
             base.OnApplyTemplate();
         }
