46094c01e74d7aebb516ce46c5b6df2f74d6c28f
SimpleBear in developer mode <81253203+itsWindows11@users.noreply.github.com>
2022-05-30 12:27:40 +0200

    Improve XAML formatting + persistent new recording bar (#118)

--- FluentScreenRecorder/Views/VideoPreviewPage.xaml.cs
+++ FluentScreenRecorder/Views/VideoPreviewPage.xaml.cs
@@ -19,13 +19,16 @@ namespace FluentScreenRecorder.Views
         public VideoPreviewPage(StorageFile file = null)
         {
             InitializeComponent();
-            SetupTitleBar();
+
+            MainPage.Current.SettingsButton.Visibility = Visibility.Collapsed;
 
             if (file != null)
             {
                 _tempFile = file;
             }                
             PreviewPlayer.Source = MediaSource.CreateFromStorageFile(file);
+
+            ApplicationView.GetForCurrentView().TryResizeView(new(500, 500));
 
             DataTransferManager dataTransferManager = DataTransferManager.GetForCurrentView();
             dataTransferManager.DataRequested += new TypedEventHandler<DataTransferManager, DataRequestedEventArgs>(DataRequested);            
@@ -34,54 +37,10 @@ namespace FluentScreenRecorder.Views
         public VideoPreviewPage()
         {
             InitializeComponent();
-            SetupTitleBar();
-        }
 
-        private void SetupTitleBar(CoreApplicationViewTitleBar coreAppTitleBar = null)
-        {
-            var coreTitleBar = coreAppTitleBar ?? CoreApplication.GetCurrentView().TitleBar;
-            coreTitleBar.ExtendViewIntoTitleBar = true;
+            MainPage.Current.SettingsButton.Visibility = Visibility.Collapsed;
 
-            // Get the size of the caption controls area and back button 
-            // (returned in logical pixels), and move your content around as necessary.
-            LeftPaddingColumn.Width = new GridLength(coreTitleBar.SystemOverlayLeftInset);
-            RightPaddingColumn.Width = new GridLength(coreTitleBar.SystemOverlayRightInset);
-
-            // Set XAML element as a draggable region.
-            Window.Current.SetTitleBar(UserLayout);
-
-            // Register a handler for when the size of the overlaid caption control changes.
-            // For example, when the app moves to a screen with a different DPI.
-            coreTitleBar.LayoutMetricsChanged += OnTitleBarLayoutMetricsChanged;
-
-            //Display the right icon for exiting/entering Overlay Mode
-            if (ApplicationView.GetForCurrentView().ViewMode == ApplicationViewMode.Default)
-            {
-                ExitOverlayIcon.Visibility = Visibility.Collapsed;
-                GoToOverlayIcon.Visibility = Visibility.Visible;
-                ToolTip toolTip = new()
-                {
-                    Content = Strings.Resources.GoToOverlay
-                };
-                ToolTipService.SetToolTip(OverlayButton, toolTip);
-                AutomationProperties.SetName(OverlayButton, Strings.Resources.GoToOverlay);
-            }
-            else
-            {
-                ExitOverlayIcon.Visibility = Visibility.Visible;
-                GoToOverlayIcon.Visibility = Visibility.Collapsed;
-                ToolTip toolTip = new()
-                {
-                    Content = Strings.Resources.ExitOverlay
-                };
-                ToolTipService.SetToolTip(OverlayButton, toolTip);
-                AutomationProperties.SetName(OverlayButton, Strings.Resources.ExitOverlay);
-            }
-        }
-
-        public void OnTitleBarLayoutMetricsChanged(CoreApplicationViewTitleBar sender, object args)
-        {
-            SetupTitleBar(sender);
+            ApplicationView.GetForCurrentView().TryResizeView(new(500, 500));
         }
 
         private void DataRequested(DataTransferManager sender, DataRequestedEventArgs e)
@@ -110,13 +69,17 @@ namespace FluentScreenRecorder.Views
         private async void SaveButton_Click(object sender, RoutedEventArgs e)
         {
             await MainPage.Save(_tempFile);
-            Frame.Navigate(typeof(MainPage));
+            await MainPage.Current.LoadThumbanails();
+            Frame.Visibility = Visibility.Collapsed;
+            MainPage.Current.SettingsButton.Visibility = Visibility.Visible;
         }
 
         private async void SaveAsButton_Click(object sender, RoutedEventArgs e)
         {
             await MainPage.SaveAs(_tempFile);
-            Frame.Navigate(typeof(MainPage));
+            await MainPage.Current.LoadThumbanails();
+            Frame.Visibility = Visibility.Collapsed;
+            MainPage.Current.SettingsButton.Visibility = Visibility.Visible;
         }
 
         private void Share_Click(object sender, RoutedEventArgs e)
@@ -127,43 +90,8 @@ namespace FluentScreenRecorder.Views
         private async void Cancel_Click(object sender, RoutedEventArgs e)
         {
             await MainPage.Delete(_tempFile);
-            Frame.Navigate(typeof(MainPage));
-        }
-
-        private async void OverlayButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (ApplicationView.GetForCurrentView().ViewMode == ApplicationViewMode.Default)
-            {
-                var preferences = ViewModePreferences.CreateDefault(ApplicationViewMode.CompactOverlay);
-                preferences.CustomSize = new Size(400, 260);
-                bool modeSwitched = await ApplicationView.GetForCurrentView().TryEnterViewModeAsync(ApplicationViewMode.CompactOverlay, preferences);
-                if (modeSwitched)
-                {
-                    GoToOverlayIcon.Visibility = Visibility.Collapsed;
-                    ExitOverlayIcon.Visibility = Visibility.Visible;
-                    ToolTip toolTip = new()
-                    {
-                        Content = Strings.Resources.ExitOverlay
-                    };
-                    ToolTipService.SetToolTip(OverlayButton, toolTip);
-                    AutomationProperties.SetName(OverlayButton, Strings.Resources.ExitOverlay);
-                }
-            }
-            else
-            {
-                bool modeSwitched = await ApplicationView.GetForCurrentView().TryEnterViewModeAsync(ApplicationViewMode.Default);
-                if (modeSwitched)
-                {
-                    ExitOverlayIcon.Visibility = Visibility.Collapsed;
-                    GoToOverlayIcon.Visibility = Visibility.Visible;
-                    ToolTip toolTip = new()
-                    {
-                        Content = Strings.Resources.GoToOverlay
-                    };
-                    ToolTipService.SetToolTip(OverlayButton, toolTip);
-                    AutomationProperties.SetName(OverlayButton, Strings.Resources.GoToOverlay);
-                }
-            }
+            Frame.Visibility = Visibility.Collapsed;
+            MainPage.Current.SettingsButton.Visibility = Visibility.Visible;
         }
 
         private void Page_Unloaded(object sender, RoutedEventArgs e)
@@ -171,5 +99,4 @@ namespace FluentScreenRecorder.Views
             PreviewPlayer.Source = null;
         }
     }
-       
 }
